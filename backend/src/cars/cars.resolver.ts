3ebe2235d0c2d705f0a09920c4e5ff37ec51edbe
mamoru igamoto <stream99990@yahoo.co.jp>
2021-09-12 01:42:47 +0900

    update: resolverを作成しました

---
+++ backend/src/cars/cars.resolver.ts
@@ -0,0 +1,25 @@
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Car } from './car';
+
+const carTable = [
+  {
+    id: '1',
+    title: '鯖の味噌煮',
+  },
+  {
+    id: '2',
+    title: 'ミートソーススパゲティ',
+  },
+  {
+    id: '3',
+    title: '豚の生姜焼',
+  },
+];
+
+@Resolver('Cars')
+export class CarsResolver {
+  @Query(returns => [Car])
+  async cars(): Promise<Car[]> {
+    return carTable;
+  }
+}
