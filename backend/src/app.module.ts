45303c236e0c5ba1526429b07da70f87c97b0471
mamoru igamoto <stream99990@yahoo.co.jp>
2021-09-12 01:41:55 +0900

    update: restからgqlに変更してmoduleを作成して追加しました

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    CarsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
