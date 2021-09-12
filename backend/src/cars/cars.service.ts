e014e6a7b8a31b3b7981c5f60a9e659044923801
mamoru igamoto <stream99990@yahoo.co.jp>
2021-09-12 20:58:00 +0900

    update: serviceにダミーデータとCRUDを追加しました

--- backend/src/cars/cars.service.ts
+++ backend/src/cars/cars.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { Car } from './car';
+import { NewCarInput } from './dto/newCar.input';
+
+let cars = [
+  {
+    id: 1,
+    brand: 'Alfa Romeo',
+    carName: '4C Gr.3',
+    category: 'Gr.3',
+    createdAt: new Date(),
+  },
+  {
+    id: 2,
+    brand: 'BMW',
+    carName: 'BMW M3 Coupé',
+    category: 'N 400',
+    createdAt: new Date(),
+  },
+  {
+    id: 3,
+    brand: 'Ferrari',
+    carName: 'LaFerrari',
+    category: 'N1000',
+    createdAt: new Date(),
+  },
+] as Car[];
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+  findAll(): Promise<Car[]> {
+    return Promise.resolve(cars);
+  }
+
+  findOneById(id: number): Promise<Car> {
+    const car = cars.find((car) => car.id === id);
+    return Promise.resolve(car);
+  }
+
+  create(data: NewCarInput): Promise<Car> {
+    const car: Car = {
+      id: Date.now(),
+      ...data,
+      createdAt: new Date(),
+    };
+    cars.push(car);
+
+    return Promise.resolve(car);
+  }
+
+  async remove(id: number): Promise<boolean> {
+    cars = cars.filter((car) => car.id !== id);
+    return true;
+  }
+}
