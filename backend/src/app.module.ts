3f4fbc6247044c2e02730d403b85fdd801479641
mamoru igamoto <stream99990@yahoo.co.jp>
2021-09-12 22:16:12 +0900

    update: 順番を変更しました

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 import { DatabaseModule } from './database/database.module';
 
