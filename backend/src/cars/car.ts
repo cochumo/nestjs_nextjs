cc959971aa4585a90b2ae0d895dfc18490ea54c9
mamoru igamoto <stream99990@yahoo.co.jp>
2021-09-25 18:02:56 +0900

    update: 途中までコミット

--- backend/src/cars/car.ts
+++ backend/src/cars/car.ts
@@ -1,22 +1,34 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
 
 @ObjectType()
 export class Car {
+  @PrimaryGeneratedColumn()
   @Field(() => ID)
   id: number;
 
+  @Column()
   @Field()
   brand: string;
 
+  @Column()
   @Field()
   carName: string;
 
+  @Column({ length: '15' })
   @Field()
   category: string;
 
+  @Column()
   @Field({ nullable: true })
   memo?: string;
 
+  @CreateDateColumn()
   @Field()
   createdAt: Date;
 }
